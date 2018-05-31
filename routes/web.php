f084352efd0ab4be4aa156995b716046a51486f4
kmadr20 <kmadr20.junk@googlemail.com>
2018-05-31 15:11:07 +0100

    Didn't work, so I removed testpage route and blade template.

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function () {
-    return view('testpage');
-});
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
