9061c9e5ef10999f2abda74f9d1bf5a7bc96581f
kmadr20 <kmadr20.junk@googlemail.com>
2018-06-01 09:51:19 +0100

    Quick test for deployment permissions.

--- resources/views/testpage.blade.php
+++ resources/views/testpage.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    This yo page, bitch!
+                    New test
                 </div>
 
                 <div class="links">
